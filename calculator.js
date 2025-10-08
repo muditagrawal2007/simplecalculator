a5e11cb48dd5fe299267758e3eac63dabfe85d7e
muditagrawal2007 <muditagrawal9415369961@gmail.com>
2025-10-08 22:03:44 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,4 @@
-// var conf=prompt("enter the number")
-// if (conf[1]=="+"){
-//     alert(Number(conf[0])+Number(conf[2]))
-// }
 
-// get element by the id it will take the id and it by the class name by class and if we use the query selector it will 
 function calculate() {
 
     let conf = document.getElementById("expression").value;
@@ -30,4 +25,4 @@ function calculate() {
     document.getElementById("dd").innerText = output;
 
 }
-// str.includes("+") it will check if the element is in the str or not the it will run otherwise not
+
